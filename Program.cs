ce28357c6b9fb34e1082969d2befcc41c1d35074
puglifebra <ghobart2024@chadwickschool.org>
2022-01-27 11:33:30 -0800

    stuff

--- Program.cs
+++ Program.cs
@@ -17,71 +17,71 @@ namespace _2D_Game_
         // Looking forward to seeing your work!
         // Mx. Collard
 
-        public class Waypoints
-        {
-            // Transformed used for creating an array, a list a items, which the transformed used to a list of game points
-        }
+        // public class Waypoints
+        // {
+        //     // Transformed used for creating an array, a list a items, which the transformed used to a list of game points
+        // }
 
-        public class Enemy 
-        {
-            // Float speed is meant to express the speed of the AI 
-            // Then there is a target, which is private so the enemy only have it in their script
-            // The current waypoint that the AI is going too is sown in wavepointIndex
-        }
+        // public class Enemy 
+        // {
+        //     // Float speed is meant to express the speed of the AI 
+        //     // Then there is a target, which is private so the enemy only have it in their script
+        //     // The current waypoint that the AI is going too is sown in wavepointIndex
+        // }
 
-        void start()
-         {
-            // The game starts
-            // The AI , at the start of the game, will then go to waypoint 0 
-         }
+        // void start()
+        //  {
+        //     // The game starts
+        //     // The AI , at the start of the game, will then go to waypoint 0 
+        //  }
 
-        void update()
-        {
-            // Create a direction the enemy is pointing 
-            // Have the enemy move with the vector, using the variables of time and speed
-            // When the enemy is at the waypoint which is a specific point
-            // The enemy will get the next wave point
-        }
+        // void update()
+        // {
+        //     // Create a direction the enemy is pointing 
+        //     // Have the enemy move with the vector, using the variables of time and speed
+        //     // When the enemy is at the waypoint which is a specific point
+        //     // The enemy will get the next wave point
+        // }
 
-        void GetNextWaypoint()
-        {
-            // If the wave point is trying to go to one not existing 
-            // Then Destroy the Enemy 
-            // The wave point index will increase by one 
-            // The target is going to be the new wave point that is given from above code
-        }
+        // void GetNextWaypoint()
+        // {
+        //     // If the wave point is trying to go to one not existing 
+        //     // Then Destroy the Enemy 
+        //     // The wave point index will increase by one 
+        //     // The target is going to be the new wave point that is given from above code
+        // }
 
-        void spawnerupdate()
-        {
-            // If the time for the countdown is 0 
-            // Spawn the wave and set countdown to normal time 
-            // If not then countdown will decrease by one second.
-        }
+        // void spawnerupdate()
+        // {
+        //     // If the time for the countdown is 0 
+        //     // Spawn the wave and set countdown to normal time 
+        //     // If not then countdown will decrease by one second.
+        // }
 
-        IEnumeratorsSpawnWave()
-        {
-            // So when it spawns, increase the wave number by one 
-            // So each time the wave number is 0
-            // Spawn Enemy 
-            // Then wait for the time between waves 
-        }
+        // IEnumeratorsSpawnWave()
+        // {
+        //     // So when it spawns, increase the wave number by one 
+        //     // So each time the wave number is 0
+        //     // Spawn Enemy 
+        //     // Then wait for the time between waves 
+        // }
 
-        void SpawnEnemy()
-        {
-            // We are going to make the enemy spawn at a specific location and correct direction
-        }
+        // void SpawnEnemy()
+        // {
+        //     // We are going to make the enemy spawn at a specific location and correct direction
+        // }
 
-        void IntPoints(Transform [] Points)
-        {
-            // If there are no more points 
-            // Go to void awake 
-        }
+        // void IntPoints(Transform [] Points)
+        // {
+        //     // If there are no more points 
+        //     // Go to void awake 
+        // }
 
-        void Awake()
-        {
-            // The points change the amount of points
-            // And then the new points are created. 
-        }
+        // void Awake()
+        // {
+        //     // The points change the amount of points
+        //     // And then the new points are created. 
+        // }
 
 
     }
